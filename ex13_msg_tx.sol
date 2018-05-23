21140f814b9682dc47169c4e99c0569d1b68724e
Jacques Chevrier <34470570+jacchevr@users.noreply.github.com>
2018-05-23 09:40:00 -0600

    Update ex13_msg_tx.sol

--- ex13_msg_tx.sol
+++ ex13_msg_tx.sol
@@ -1,1 +1,16 @@
-# example13-solidity
+pragma solidity ^0.4.21;
+
+contract MsgTx {
+    function MsgTx() public {
+        MsgTxCalled secondContract = new MsgTxCalled();
+        secondContract.callMe();
+    }
+}
+
+contract MsgTxCalled {
+    event MyLog(address _callerMsg, address _callerTx);
+    
+    function callMe() public {
+        emit MyLog(msg.sender, tx.origin);
+    }
+}
